e18003f7be6d36c2ada31b9241f0ccc820524e45
Jason Benn <jasoncbenn@gmail.com>
2017-04-21 13:47:06 -0300

    Pulls page and HTML.

--- src/inject/pulls.js
+++ src/inject/pulls.js
@@ -1,30 +1,22 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+  	if (document.readyState === "complete") {
+  		clearInterval(readyStateCheckInterval);
 
-		console.log("This is the list of Minerva PRs!");
+      var badge = chrome.runtime.getURL('src/templates/badge.html')
 
-    var badge = chrome.runtime.getURL('src/templates/badge.html')
-    $.get(badge).done(function(template) {
-      console.log('waiting on arrive');
-      // $(document).arrive("#issue_1834", {onceOnly: true, existing: true}, function() {
-        console.log('arrived');
-        console.log($('#issue_1834'));
-        $('#issue_1834 > .d-table').append(template)
-      // });
-    })
+      // var prStatusPromise = $.get('https://qa-instance-coordinator.minervaproject.com/pulls')
+      // prStatusPromise.done(function(data) { 
+      var prs = [2290, 2206, 2187]
+      prs.forEach(function(prId) {
+        getTemplate("badge").done(function(template) {
+          var html = template({
+            url: "https://qa-features-lo-detail-page.minervaproject.com/app/login"
+          })
+          $('#issue_'+prId+' > .d-table').append(html)
+        })
+      })
 
-
-
-    // $.get('qa-instance-coordinator.minervaproject.com/pulls').done(function() {
-    //   $('.pull-requests').forEach(function() {
-    //     // check if a pull matches, if so, insert a badge.
-    //   })
-    // })
-
-	}
+  	}
 	}, 10);
 });
-
-// TEST: that this works when going backwards through pages
