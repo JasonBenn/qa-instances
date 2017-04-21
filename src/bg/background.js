c8776bea46cdc921a2cbb3132e5d57e9e332ecd1
Jason Benn <jasoncbenn@gmail.com>
2017-04-21 13:46:27 -0300

    Inject more scripts.

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,23 +1,37 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
 var PULLS = /.*:\/\/github.com\/minervaproject\/.*\/pulls/
 var PULL = /.*:\/\/github.com\/minervaproject\/.*\/pull\/.*/
+
+console.log('connecting...');
+var socket = io.connect('https://qa-instance-coordinator.minervaproject.com/');
+
+socket.on('pulls', function(msg){
+  console.log('pulls message:', msg);
+});
+
+socket.on('pull', function(msg){
+  console.log('pull message:', msg);
+});
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(_.throttle(function(details) {
   if (PULLS.test(details.url)) {
     chrome.tabs.executeScript(details.tabId, { file: "js/jquery/jquery.min.js" }, function() {
-      chrome.tabs.executeScript(details.tabId, { file: "src/inject/pulls.js" });
+      chrome.tabs.executeScript(details.tabId, { file: "js/underscore/underscore-min.js" }, function() {
+        chrome.tabs.executeScript(details.tabId, { file: "src/utils.js" }, function() {
+          chrome.tabs.executeScript(details.tabId, { file: "src/inject/pulls.js" });
+        });
+      });
     });
     chrome.tabs.insertCSS(details.tabId, { file: "src/inject/pulls.css" });
   } else if (PULL.test(details.url)) {
     chrome.tabs.executeScript(details.tabId, { file: "js/jquery/jquery.min.js" }, function() {
-      chrome.tabs.executeScript(details.tabId, { file: "src/inject/pull.js" });
+      chrome.tabs.executeScript(details.tabId, { file: "js/underscore/underscore-min.js" }, function() {
+        chrome.tabs.executeScript(details.tabId, { file: "src/utils.js" }, function() {
+          chrome.tabs.executeScript(details.tabId, { file: "src/inject/pull.js" });
+        });
+      });
     });
     chrome.tabs.insertCSS(details.tabId, { file: "src/inject/pull.css" });
+
   }
 }, 3000));
 
