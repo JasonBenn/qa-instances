6f8e81ba3ce96c28f32a59ad848fcad5becd09f5
Jason Benn <jasoncbenn@gmail.com>
2017-04-21 13:47:13 -0300

    Pull page and HTML.

--- src/inject/pull.js
+++ src/inject/pull.js
@@ -1,26 +1,43 @@
 function getPrId() {
-  return /.../.exec(location.href)
+  return /\/(\d+)$/.exec(location.href)[1]
 }
 
 function getLatestSha() {
-
+  return "acefee"
 }
 
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
 
-    // GET pull with current PR
-    // IF pull exists:
-      // RENDER current
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-    // var startButton = chrome.runtime.getFromUrl('src/templates/')
-    // console.log(startButton);
-    // window.startButton = startButton
+      var prStatusPromise = $.get('https://qa-instance-coordinator.minervaproject.com/pulls/' + getPrId())
+      prStatusPromise.done(function(data) { 
+        console.log(data, _.size(data));
+        if (_.size(data)) {
+          getTemplate("create").done(function(template) {
+            console.log('create');
+          })
+        } else {
+          getTemplate("current").done(function(template) {
+            var html = template({
+              newerShaAvailable: false, // getLatestSha() !== data.sha,
+              url: "https://qa-features-lo-detail-page.minervaproject.com"
+            })
+            $('.mergeability-details .branch-action-item').last().after(html)
+          })
+        }
+      })
 
-		console.log("This is a single Minerva PR!");
+      // GET pull with current PR
+      // IF pull exists:
+        // RENDER current
 
-	}
+      // var startButton = chrome.runtime.getFromUrl('src/templates/')
+      // console.log(startButton);
+      // window.startButton = startButton
+
+  	}
 	}, 10);
 });
