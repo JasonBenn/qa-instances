1a830fc621fb047231f275b1bd72c5a2bd35a69e
Jason Benn <jasoncbenn@gmail.com>
2017-04-24 16:00:20 -0700

    Better templates and styles.

--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,31 @@ function getTemplate(template) {
   })
   return promise
 }
+
+function ajaxGet(url, options) {
+  return $.ajax(_.extend({
+    url: url,
+    contentType: "application/json",
+  }, options))
+}
+
+function ajaxDelete(url, options) {
+  return $.ajax(_.extend({
+    url: url,
+    type: "DELETE",
+    dataType: "json",
+    contentType: "application/json"
+  }))
+}
+
+function ajaxPost(url, data, options) {
+  return $.ajax(_.extend({
+    url: url,
+    data: JSON.stringify(data),
+    type: "POST",
+    dataType: "json",
+    contentType: "application/json"
+  }))
+}
+
+function noOp() {}
