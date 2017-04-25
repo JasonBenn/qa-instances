baed04d0fbbcbff66a077801cfb5e49d16f2480f
Jason Benn <jasoncbenn@gmail.com>
2017-04-24 18:22:28 -0700

    Finalize bg.js.

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,32 +1,27 @@
 var PULLS = /.*:\/\/github.com\/minervaproject\/.*\/pulls/
 var PULL = /.*:\/\/github.com\/minervaproject\/.*\/pull\/.*/
 
-console.log('connecting...');
-var socket = io.connect('https://qa-instance-coordinator.minervaproject.com/');
-
-socket.on('pulls', function(msg){
-  console.log('pulls message:', msg);
-});
-
-socket.on('pull', function(msg){
-  console.log('pull message:', msg);
-});
-
+// Inject JS/CSS on URL updates, because Github is a single-page app.
+// These files should match the content_scripts in manifest.json.
 chrome.webNavigation.onHistoryStateUpdated.addListener(_.throttle(function(details) {
   if (PULLS.test(details.url)) {
-    chrome.tabs.executeScript(details.tabId, { file: "js/jquery/jquery.min.js" }, function() {
-      chrome.tabs.executeScript(details.tabId, { file: "js/underscore/underscore-min.js" }, function() {
-        chrome.tabs.executeScript(details.tabId, { file: "src/utils.js" }, function() {
-          chrome.tabs.executeScript(details.tabId, { file: "src/inject/pulls.js" });
+    chrome.tabs.executeScript(details.tabId, { file: "js/socket.io.js" }, function() {
+      chrome.tabs.executeScript(details.tabId, { file: "js/jquery/jquery.min.js" }, function() {
+        chrome.tabs.executeScript(details.tabId, { file: "js/underscore/underscore-min.js" }, function() {
+          chrome.tabs.executeScript(details.tabId, { file: "src/utils.js" }, function() {
+            chrome.tabs.executeScript(details.tabId, { file: "src/inject/pulls.js" });
+          });
         });
       });
     });
     chrome.tabs.insertCSS(details.tabId, { file: "src/inject/pulls.css" });
   } else if (PULL.test(details.url)) {
-    chrome.tabs.executeScript(details.tabId, { file: "js/jquery/jquery.min.js" }, function() {
-      chrome.tabs.executeScript(details.tabId, { file: "js/underscore/underscore-min.js" }, function() {
-        chrome.tabs.executeScript(details.tabId, { file: "src/utils.js" }, function() {
-          chrome.tabs.executeScript(details.tabId, { file: "src/inject/pull.js" });
+    chrome.tabs.executeScript(details.tabId, { file: "js/socket.io.js" }, function() {
+      chrome.tabs.executeScript(details.tabId, { file: "js/jquery/jquery.min.js" }, function() {
+        chrome.tabs.executeScript(details.tabId, { file: "js/underscore/underscore-min.js" }, function() {
+          chrome.tabs.executeScript(details.tabId, { file: "src/utils.js" }, function() {
+            chrome.tabs.executeScript(details.tabId, { file: "src/inject/pull.js" });
+          });
         });
       });
     });
@@ -34,10 +29,3 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(_.throttle(function(detai
 
   }
 }, 3000));
-
-//example of using a message handler from the inject scripts
-// chrome.extension.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//   	chrome.pageAction.show(sender.tab.id);
-//     sendResponse();
-//   });
