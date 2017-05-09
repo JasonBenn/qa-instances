6b48564f93e39824b05fafd37cad6eb00e4243c7
Jason Benn <jasoncbenn@gmail.com>
2017-05-09 12:12:04 -0700

    Simplify states.

--- src/config.js
+++ src/config.js
@@ -3,16 +3,9 @@ var LOCAL = false
 var BASE_URL = "https://qa-instance-coordinator" + (LOCAL ? "-local" : "") + ".minervaproject.com"
 
 var States = {
-  Instance: {
-    OFFLINE: "offline",
-    STARTING: "starting",
-    ONLINE: "online",
-    STOPPING: "stopping"
-  },
-  DB: {
-    OFFLINE: "offline",
-    STARTING: "starting",
-    ONLINE: "online",
-    ERROR: "error"
-  }
+  OFFLINE: "offline",
+  STARTING: "starting",
+  ONLINE: "online",
+  STOPPING: "stopping",
+  ERROR: "error"
 }
