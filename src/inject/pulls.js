d1b81a31cb0344da9b60f11c0b0f5a5b02005022
Jason Benn <jasoncbenn@gmail.com>
2017-05-17 15:01:22 -0700

    Instances that transition to "online" on the pulls page via WebSocket displayed broken links.

--- src/inject/pulls.js
+++ src/inject/pulls.js
@@ -1,24 +1,40 @@
 var socket = io.connect(BASE_URL);
 monkeyPatchWebsocketClient(socket)
 
-function render(prId, template, state, domainName) {
-  var color, url
+var state = {}
 
-  if (state === States.Online) {
-    url = "https://" + domainName
+var initialPrState = {
+  overallState: "",
+  domainName: ""
+}
+
+function render(prId, template) {
+  var color, url
+  var pr = state[prId]
+
+  if (pr.overallState === States.Online) {
+    url = "https://" + pr.domainName
   } else {
     url = getUrlOfBottomOfPrPage(prId)
   }
 
-  if (state === "starting" || state === "stopping") {
+  if (pr.overallState === "starting" || pr.overallState === "stopping") {
     color = "yellow"
-  } else if (state === "error") {
+  } else if (pr.overallState === "error") {
     color = "red"
-  } else if (state === "online") {
+  } else if (pr.overallState === "online") {
     color = "green"
   }
 
   $('#issue_' + prId + ' > .d-table').append(template({ url: url, color: color }))
+}
+
+function updateStateAndRender(template, prId, prData) {
+  // Filter out any key/value pairs with undefined values.
+  var stateUpdates = _.omit(prData, _.isUndefined)
+  state[prId] = state[prId] || _.extend({}, initialPrState)
+  _.extend(state[prId], stateUpdates)
+  render(prId, template)
 }
 
 chrome.extension.sendMessage({}, function(response) {
@@ -30,19 +46,19 @@ chrome.extension.sendMessage({}, function(response) {
       var badgePromise = getTemplate('badge')
 
       $.when(prsPromise, badgePromise).done(function(prs, template) {
+        var boundUpdateAndRender = updateStateAndRender.bind(this, template)
+
         prs[0].data.forEach(function(pr) {
-          render(pr.prId, template, pr.overallState, pr.domainName)
+          boundUpdateAndRender(pr.prId, { overallState: pr.overallState, domainName: pr.domainName })
         })
         
         function onMessage(template, channel, rawMessage) {
-          var message = JSON.parse(rawMessage)
           var prId = /\d+$/.exec(channel)[0]
-          var state = message.overallState
-          var domainName = message.domainName
+          var msg = JSON.parse(rawMessage)
 
-          if (state || domainName) {
-            console.log('ps:', channel, message)
-            render(prId, template, state, domainName)
+          if (msg.overallState || msg.domainName) {
+            console.log('ps:', channel, msg)
+            boundUpdateAndRender(prId, { overallState: msg.overallState, domainName: msg.domainName })
           }
         }
 
